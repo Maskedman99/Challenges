0dc29c318bac82e96878b28de96ceab93580baec
Rohit <rohitmpaul@gmail.com>
2020-05-04 20:34:07 +0530

    236 done

---
+++ 200-249/236.RGBtoHexConversion.py
@@ -0,0 +1,30 @@
+# https://dev.to/thepracticaldev/daily-challenge-236-rgb-to-hex-conversion-2iep
+
+'''
+This rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned.
+Valid decimal values for RGB are 0 - 255.
+Any values that fall out of that range must be rounded to the closest valid value.
+The following are examples of expected output values:
+
+rgb(255, 255, 255) # returns FFFFFF
+rgb(255, 255, 300) # returns FFFFFF
+rgb(0,0,0) # returns 000000
+rgb(148, 0, 211) # returns 9400D3
+
+Tests:
+rgb(-20,275,125)
+rgb(255,255,255)
+'''
+r = int(input("R: "))
+g = int(input("G: "))
+b = int(input("B: "))
+
+def rgb(var):
+	if var < 0:
+		var = 0
+	if var > 255:
+		var = 255
+
+	return hex(var).split('x')[-1].zfill(2)
+
+print('#',rgb(r), rgb(g), rgb(b), sep='')
